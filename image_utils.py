4cdb25ff3326469d87db73a35c702cc956730bb3
Jeffrey Sherlock <jeffreysherlock@Jeffreys-Mac-mini.local>
2016-11-15 08:20:43 -0800

    fixes

--- image_utils.py
+++ image_utils.py
@@ -1,15 +1,16 @@
 # -*- coding: utf-8 -*-
 import requests, sys, json
 import requests_cache
-import hashlib
 import config
 from datetime import datetime
-import urllib, json, pytz, dateutil.parser, time
+import urllib, json, pytz, dateutil.parser, time, os
 from wand.image import Image
 from wand.display import display
 from wand.drawing import Drawing
 from wand.color import Color
-
+import boto
+from boto.s3.key import Key
+from boto.s3.connection import S3Connection, OrdinaryCallingFormat
 
 def degToCompass(num):
     val=int((num/22.5)+.5)
@@ -40,11 +41,17 @@ def get_wind(prefix, lat, lon):
 
     return (wind_speed_mph, wind_dir)
 
+def get_timezone(prefix, lat, lon):
+    location_data = get_weather_data(prefix, lat, lon)
+    location_timezone = pytz.timezone(location_data['timezone'])
+
+    return location_timezone
+
 def get_image_tag(prefix, lat, lon):
     location_data = get_weather_data(prefix, lat, lon)
     location_timezone = pytz.timezone(location_data['timezone'])
-    sunrise = datetime.fromtimestamp(location_data['daily']['data']['sunriseTime'], tz=location_timezone)
-    sunset = datetime.fromtimestamp(location_data['daily']['data']['sunsetTime'], tz=location_timezone)
+    sunrise = datetime.fromtimestamp(location_data['daily']['data'][0]['sunriseTime'], tz=location_timezone)
+    sunset = datetime.fromtimestamp(location_data['daily']['data'][0]['sunsetTime'], tz=location_timezone)
 
     u = datetime.now(tz=location_timezone)
 
@@ -70,36 +77,55 @@ def get_image_tag(prefix, lat, lon):
     return tag
 
 def create_filename(prefix, lat, lon):
-    current_time_str = datetime.now().strftime('%Y-%m-%d_%H:%M:%S')
+    current_time_str = datetime.now(tz=get_timezone(prefix, lat, lon)).strftime('%Y-%m-%d_%H:%M:%S')
     tag = get_image_tag(prefix, lat, lon)
-    return "%s_%s_%s" % (prefix, current_time_str, tag)
+    return "%s_%s_%s.jpg" % (prefix, current_time_str, tag)
 
 def fetch_original_image(url, prefix):
-    urllib.urlretrieve(url, hashlib.md5(prefix + datetime.datetime.now()))
+    urllib.urlretrieve(url, prefix + '.jpg')
 
-def overlay_weather(prefix, lat, lon):
-    temp_data = get_temp()
-    wind_data = get_wind()
-    output_filename = create_filename(prefix, lat, lon)
+def overlay_weather(prefix, lat, lon, output_filename):
+    temp_data = get_temp(prefix, lat, lon)
+    wind_data = get_wind(prefix, lat, lon)
     
     temp_str = str(temp_data[0])+"° F | "+str(temp_data[1])+ "° C"
     wind_str = str(wind_data[0])+" MPH "+wind_data[1]
 
-    with Image(filename=hashlib.md5(output_filename)) as img:
+    with Image(filename=prefix+'.jpg') as img:
         with Drawing() as draw:
             draw.fill_color=Color('#292929')
-            draw.fill_opacity = 0.2
-            draw.rectangle(left=30, top=1810, width=340, height=100)
+            draw.fill_opacity = 0.4
+            draw.rectangle(left=30, top=1810, width=300, height=90)
             draw.draw(img)
-            draw.fontfamily = 'CourierNew'
-            draw.font_size = 42
+            draw.fontfamily = 'Monaco'
+            draw.font_size = 36
             draw.fill_color = Color('White')
             draw.gravity = 'south_west'
             draw.text(30,17,wind_str)
             draw.text(30,60,temp_str)
             draw.draw(img)
-            img.save(filename=create_filename)
+            img.save(filename=output_filename)
+
+def save_image_to_s3(filename, loc_timezone, bucket_name):
+    conn = S3Connection(config.AWS_ACCESS_KEY, config.AWS_SECRET_KEY, host=config.AWS_S3_HOST, calling_format=OrdinaryCallingFormat())
+    bucket = conn.get_bucket(bucket_name, validate=False)
+    cur_date= datetime.now(tz=loc_timezone).strftime('%Y-%m-%d')
+    key_str = "test/%s/%s" % (cur_date, filename)
+    k = Key(bucket)
+    k.key = key_str
+    k.set_contents_from_filename(filename)
 
 def process_image(location_config):
+    if int(time.time()) % location_config['interval'] != 0:
+        return
+    filename = create_filename(location_config['prefix'], location_config['lat'], location_config['lon'])
     fetch_original_image(location_config['url'], location_config['prefix'])
-    overlay_weather(location_config['prefix'], location_config['lat'], location_config['lon'])
+    overlay_weather(location_config['prefix'], location_config['lat'], location_config['lon'], filename)
+    timezone = get_timezone(location_config['prefix'], location_config['lat'], location_config['lon'])
+    save_image_to_s3(filename, timezone, location_config['s3_bucket'])
+    os.remove(filename)
+
+for location_config in config.IMAGE_LOCATIONS:
+    process_image(location_config)
+
+
