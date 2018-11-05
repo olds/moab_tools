dbfcc7df0d1386e2f629cbb8eefb430f3ce2ec0f
Jeff <jeff@labgopher.com>
2018-11-04 17:21:48 -0700

    configuration example

---
+++ config_example.py
@@ -0,0 +1,17 @@
+from lib import Location
+
+DARKSKY_API_KEY = 'xxx'
+
+#DigitalOcean Spaces Keys go here
+ACCESS_ID = ''
+SECRET_KEY = ''
+IMAGE_LOCATIONS = [
+    Location(resource_url='url goes here',
+             lat='40.573315',
+             lon='-120.549843',
+             overlay_weather=True,
+             overlay_title=True,
+             title = 'A Place',
+             prefix='a_place'),
+]
+
